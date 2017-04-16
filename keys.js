6504f3624d5c49963a541070b21b0108af8c1d72
dasseya1 <dasseya1@something.com>
2017-04-16 04:13:29 -0400

    Added comments and tested app

--- keys.js
+++ keys.js
@@ -2,10 +2,10 @@ console.log('this is loaded');
 
 //these are the twitter API credentials
 exports.twitterKeys = {
-  consumer_key: 'xwjk9BHCXcRTIuA11NzVQp9Hn',
-  consumer_secret: 'E713okVVwHle4eTwloNjtesDuD1eJmwNwNyPZ2iIcWDylO00Mw',
-  access_token_key: '711243531130548224-YY41kYUKJ6ObbpBvKhVmIwbKbt6sdhV',
-  access_token_secret: 'G1rRxfzY010F4Jfd5ypHeI2svLch0VBqdBk4nEHNZBYEz',
+    consumer_key: 'xwjk9BHCXcRTIuA11NzVQp9Hn',
+    consumer_secret: 'E713okVVwHle4eTwloNjtesDuD1eJmwNwNyPZ2iIcWDylO00Mw',
+    access_token_key: '711243531130548224-YY41kYUKJ6ObbpBvKhVmIwbKbt6sdhV',
+    access_token_secret: 'G1rRxfzY010F4Jfd5ypHeI2svLch0VBqdBk4nEHNZBYEz',
 }
 
 
--- liri.js
+++ liri.js
@@ -7,31 +7,31 @@ var client = new Twitter(twitterCredentials.twitterKeys);
 
 //These are the parameters for the API call
 var params = {
-	screen_name: 'cpaquestions',
-	      count: 20
+    screen_name: 'cpaquestions',
+    count: 20
 };
 
 //Perform the API call and return data
 client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
-  var dataToFile;
-  if (!error) {
-  	for (var i = 0; i < tweets.length; i++) {
-  		console.log(tweets[i].created_at + " " + tweets[i].text);
-  		console.log("-------------------------------");
-  		dataToFile = tweets[i].created_at + " " + tweets[i].text + "\n-----------\n";
- // This block of code will append tweets to the file called "log.txt".
- 	fs.appendFile("log.txt", dataToFile, function(err) {
+    var dataToFile;
+    if (!error) {
+        for (var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].created_at + " " + tweets[i].text);
+            console.log("-------------------------------");
+            
+            dataToFile = tweets[i].created_at + " " + tweets[i].text + "\n-----------\n";
+            
+            //This block of code will append tweets to the file called "log.txt".
+            fs.appendFile("log.txt", dataToFile, function(err) {
 
-   // If the code experiences any errors it will log the error to the console.
-   if (err) {
-     return console.log(err);
-   }
- 		})
-  	}
-  }
+                // If the code experiences any errors it will log the error to the console.
+                if (err) {
+                    return console.log(err);
+                }
+            })
+        }
+    }
 
-  console.log("Last 20 tweets are now displayed on the screen and saved in log.txt.");
+    console.log("Last 20 tweets are now displayed on the screen and saved in log.txt.");
 
 });
-
-
